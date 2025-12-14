f1f96076963cae9fc67d314bdc7d965e5e09fbd2
Jan <jan.trebecki@gmail.com>
2025-12-14 12:56:39 +0100

    14.12

--- src/Test_class.php
+++ src/Test_class.php
@@ -1,11 +1,17 @@
-?php
+<?php
 
 declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 class TestController extends AbstractController
 {
-    
+    #[Route ("/testowy", "Test")]
+    public function index (): Response
+    {
+        return new Response('Hello World!');
+    }
 }
